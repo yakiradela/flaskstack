080612558d35bb0c0564203737c2d07dc36eb0fe
Yakir Adela <yakirade@gmail.com>
2025-06-06 09:36:33 +0300

    Update main.tf

--- flaskstack/infra/main.tf
+++ flaskstack/infra/main.tf
@@ -17,8 +17,11 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 
-  create_kms_key = false
-  kms_key_id     = aws_kms_key.eks.arn
+  cluster_encryption_config = {
+    resources = ["secrets"]
+    provider  = "aws"
+    key_arn   = aws_kms_key.eks.arn
+  }
 
   eks_managed_node_groups = {
     default = {
