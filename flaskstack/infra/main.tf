8d8956453ef2e99e7400b01b76b21e71b23d9951
Yakir Adela <yakirade@gmail.com>
2025-06-07 12:24:03 +0300

    Update main.tf

--- flaskstack/infra/main.tf
+++ flaskstack/infra/main.tf
@@ -7,6 +7,22 @@ module "vpc" {
   public_subnets         = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets        = ["10.0.3.0/24", "10.0.4.0/24"]
   enable_nat_gateway     = true
+  single_nat_gateway     = true
+  enable_dns_hostnames   = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb" = "1"
+  }
 }
 
 module "eks" {
