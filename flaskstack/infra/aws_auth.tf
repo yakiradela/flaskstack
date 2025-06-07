82e862c1674fdcc5aafbb16e01af484af204ea9f
Yakir Adela <yakirade@gmail.com>
2025-06-07 18:01:32 +0300

    Update aws_auth.tf

--- flaskstack/infra/aws_auth.tf
+++ flaskstack/infra/aws_auth.tf
@@ -1,21 +1,2 @@
-resource "null_resource" "aws_auth" {
-  provisioner "local-exec" {
-    command = <<EOT
-      aws eks update-kubeconfig --region ${var.aws_region} --name ${var.cluster_name}
-      kubectl apply -f - <<EOF
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: aws-auth
-  namespace: kube-system
-data:
-  mapUsers: |
-    - userarn: arn:aws:iam::557690607676:user/flaskstack
-      username: flaskstack
-      groups:
-        - system:masters
-EOF
-    EOT
-  }
-}
+
 
