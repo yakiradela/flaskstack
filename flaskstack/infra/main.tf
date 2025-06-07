f1c20e6d8a764f451824301461c74289ae1404c9
Yakir Adela <yakirade@gmail.com>
2025-06-07 14:49:33 +0300

    Update main.tf

--- flaskstack/infra/main.tf
+++ flaskstack/infra/main.tf
@@ -1,9 +1,10 @@
+
 module "vpc" {
   source                 = "terraform-aws-modules/vpc/aws"
   version                = "5.0.0"
-  name                   = "vpc"
+  name                   = "flaskstack-vpc"
   cidr                   = "10.0.0.0/16"
-  azs                    = ["us-east-2a", "us-east-2b"]
+  azs                    = ["${var.aws_region}a", "${var.aws_region}b"]
   public_subnets         = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets        = ["10.0.3.0/24", "10.0.4.0/24"]
   enable_nat_gateway     = true
@@ -12,16 +13,17 @@ module "vpc" {
 
   tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "Environment"                               = var.environment
   }
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/role/elb"                    = "1"
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/role/internal-elb"           = "1"
   }
 }
 
@@ -49,6 +51,9 @@ module "eks" {
       instance_types = ["t3.medium"]
     }
   }
-}
 
+  tags = {
+    "Environment" = var.environment
+  }
+}
 
