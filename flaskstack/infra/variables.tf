5b7ca229e5bf6e7b18eee1bedd0233678dfd26e9
Yakir Adela <yakirade@gmail.com>
2025-06-07 16:15:08 +0300

    Update variables.tf

--- flaskstack/infra/variables.tf
+++ flaskstack/infra/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "cluster_name" {
   description = "The name of the EKS cluster"
   type        = string
-  default     = "flaskstack-cluster"
+  default     = "Flaskstack-cluster"
 }
 
 variable "node_count" {
