1cb33593fc4e3051d80c2a3c0e3dae367c8ac0cf
Yakir Adela <yakirade@gmail.com>
2025-06-09 16:50:29 +0300

    Update main.tf

--- flaskstack/infra/main.tf
+++ flaskstack/infra/main.tf
@@ -55,17 +55,6 @@ module "eks" {
     }
   }
 
-  # ניהול aws-auth configmap
-  manage_aws_auth_configmap = true
-
-  aws_auth_users = [
-    {
-      userarn  = "arn:aws:iam::557690607676:user/flaskstack"
-      username = "flaskstack"
-      groups   = ["system:masters"]
-    }
-  ]
-
   tags = {
     "Environment" = var.environment
   }
@@ -79,5 +68,38 @@ resource "aws_security_group_rule" "allow_https_inbound" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.eks.node_security_group_id
   description       = "Allow inbound HTTPS traffic to EKS nodes"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapUsers = yamlencode([
+      {
+        userarn  = "arn:aws:iam::557690607676:user/flaskstack"
+        username = "flaskstack"
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+
+  depends_on = [module.eks]
 }
 
