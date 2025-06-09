47a1c51f1c8f12054013223d1d91e143306d5b13
Yakir Adela <yakirade@gmail.com>
2025-06-09 17:09:45 +0300

    Update main.tf

--- flaskstack/infra/main.tf
+++ flaskstack/infra/main.tf
@@ -37,8 +37,8 @@ module "eks" {
 
   create_kms_key = false
 
-  cluster_endpoint_public_access       = true
-  cluster_endpoint_private_access      = true
+  cluster_endpoint_public_access        = true
+  cluster_endpoint_private_access       = true
   cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
   cluster_encryption_config = {
@@ -68,38 +68,5 @@ resource "aws_security_group_rule" "allow_https_inbound" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.eks.node_security_group_id
   description       = "Allow inbound HTTPS traffic to EKS nodes"
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    mapUsers = yamlencode([
-      {
-        userarn  = "arn:aws:iam::557690607676:user/flaskstack"
-        username = "flaskstack"
-        groups   = ["system:masters"]
-      }
-    ])
-  }
-
-  depends_on = [module.eks]
 }
 
