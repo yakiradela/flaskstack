83e3e32fbe32907ae97da8f50606443e45f06bed
Yakir Adela <yakirade@gmail.com>
2025-06-10 19:09:30 +0300

    Update main.tf

--- flaskstack/infra/main.tf
+++ flaskstack/infra/main.tf
@@ -1,3 +1,4 @@
+
 module "vpc" {
   source               = "terraform-aws-modules/vpc/aws"
   version              = "5.0.0"
@@ -41,10 +42,7 @@ module "eks" {
   cluster_endpoint_private_access       = true
   cluster_endpoint_public_access_cidrs  = ["0.0.0.0/0"]
 
-  cluster_encryption_config = {
-    provider_key_arn = aws_kms_key.eks.arn
-    resources        = ["secrets"]
-  }
+  # הסרתי cluster_encryption_config כי aws_kms_key.eks לא מוגדר
 
   eks_managed_node_groups = {
     default = {
@@ -60,3 +58,16 @@ module "eks" {
   }
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
