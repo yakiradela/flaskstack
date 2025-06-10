2938c52a3fd5209f7fa2d26a4335586afedaae31
Yakir Adela <yakirade@gmail.com>
2025-06-10 16:37:45 +0300

    Update aws_auth.tf

--- flaskstack/infra/aws_auth.tf
+++ flaskstack/infra/aws_auth.tf
@@ -1,2 +1,19 @@
+module "aws_auth" {
+  source  = "terraform-aws-modules/eks/aws//modules/aws-auth"
+  version = "20.24.2"
+
+  cluster_name = module.eks.cluster_name
+  create       = true
+
+  manage_aws_auth_configmap = true
+
+  map_users = [
+    {
+      userarn  = "arn:aws:iam::557690607676:user/flaskstack"
+      username = "flaskstack"
+      groups   = ["system:masters"]
+    }
+  ]
+}
 
 
