9edc82a1738cb1e8c6b82539a5050a445c6ed908
Yakir Adela <yakirade@gmail.com>
2025-06-13 20:17:35 +0300

    Update outputs.tf

--- flaskstack/infra/outputs.tf
+++ flaskstack/infra/outputs.tf
@@ -10,3 +10,7 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
+output "ecr_repository_url" {
+  description = "The URL of the ECR repository"
+  value       = aws_ecr_repository.flaskstack.repository_url
+}
